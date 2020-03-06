76e9f7167fa4619531650b1a3196f8f43e7c3507
Elaine Oliveira <elaine.oliveira@itixti.com.br>
2020-03-06 15:56:09 -0300

    Criando um retorno em json

--- appFlask.py
+++ appFlask.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/<int:numero>", methods=['GET','POST'])
 def hello(numero):
     return u'Olá mundo!: {}'.format(numero)
+
+@app.route("/json")
+def retornoJson():
+    return jsonify({'nome': 'Elaine Cristina', 'sobrenome': 'Rocha de Oliveira'})
 
 if __name__ == "__main__":
     app.run(debug=True)
