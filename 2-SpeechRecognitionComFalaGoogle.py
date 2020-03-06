a214bb1c2affbe32c60045f6f76e00924d7d7450
ElaineCRO <elainecro@gmail.com>
2020-03-06 01:42:14 -0300

    Testes e documentos

---
+++ 2-SpeechRecognitionComFalaGoogle.py
@@ -0,0 +1,30 @@
+import speech_recognition as sr 
+from gtts import gTTS
+from playsound import playsound
+
+def cria_audio(audio):
+    tts = gTTS(audio, lang='pt-br')
+    tts.save('audios/teste.mp3')
+    print("Estou aprendendo o que você disse...")
+    playsound('audios/teste.mp3')
+
+def ouvir_microfone():
+    microfone = sr.Recognizer()
+    with sr.Microphone() as source:
+        #redução de ruído
+        microfone.adjust_for_ambient_noise(source)
+        
+        print("Diga alguma coisa: ")
+        audio = microfone.listen(source)
+        
+    try:
+        frase = microfone.recognize_google(audio, language='pt-BR')
+        print("Você disse: " + frase)
+    except sr.UnkownValueError:
+        print("Não entendi")
+        
+    return frase
+
+
+frase = ouvir_microfone()
+cria_audio(frase)
---
+++ README.md
@@ -0,0 +1,8 @@
+### Central de libs
+https://www.lfd.uci.edu/~gohlke/pythonlibs/
+Uso para baixar a lib quando dá problema no pip install
+
+### Bibliotecas para Instalar
+pip install pyaudio
+pip install pocketsphinx
+pip install SpeechRecognition
